b230821cc450cca977ab90dfc212eb6bc131cf6f
Finomnis <finomnis@gmail.com>
2024-03-08 17:22:07 +0100

    Implement low level sdep read/write

--- src/delays.rs
+++ src/delays.rs
@@ -2,5 +2,9 @@ pub use _delays::*;
 
 mod _delays {
     pub const RESET_PULSE_LENGTH_MS: u32 = 10;
-    pub const WAIT_AFTER_RESET_MS: u32 = 500;
+    pub const AFTER_RESET_MS: u32 = 500;
+    pub const AFTER_INIT_MS: u32 = 1000;
+    pub const AFTER_SDEP_READ_US: u32 = 300;
+    pub const AFTER_SDEP_WRITE_US: u32 = 300;
+    pub const CS_TO_SCK_US: u32 = 100;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 #![doc(issue_tracker_base_url = "https://github.com/Finomnis/BluefruitSPI/issues")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
+use core::fmt::Debug;
+
+use snafu::prelude::*;
+
 mod delays;
 pub mod sdep;
 
@@ -17,21 +21,33 @@ pub mod sdep;
 /// CS through GPIO.
 pub trait SpiBus {
     /// The errors that can happen in BUS communication
-    type Error;
+    type Error: snafu::AsErrorSource;
 
     /// Sends data to the bus.
+    ///
+    /// Blocks until the data was completely sent on the bus
+    /// and the device is idle again.
     ///
     /// # Arguments
     ///
     /// * `data` - The data to be sent to the bus.
-    fn send(&mut self, data: &[u8]) -> Result<(), Self::Error>;
+    fn transmit(&mut self, data: &[u8]) -> Result<(), Self::Error>;
+
+    /// Reads data from the bus.
+    ///
+    /// Blocks until the entire buffer was filled with data and the device is idle again.
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - The buffer the data should be read into.
+    fn receive(&mut self, buffer: &mut [u8; 20]) -> Result<(), Self::Error>;
 }
 
 /// Driver for Adafruit Bluefruit LE SPI Friend.
 pub struct BluefruitSPI<SPI, CS, RST, IRQ, DELAY> {
     spi: SPI,
     cs: CS,
-    reset: RST,
+    _reset: RST,
     irq: IRQ,
     delay: DELAY,
     buffer: [u8; 20],
@@ -60,17 +76,80 @@ where
         reset.set_low().unwrap();
         cassette::block_on(delay.delay_ms(delays::RESET_PULSE_LENGTH_MS));
         reset.set_high().unwrap();
-        cassette::block_on(delay.delay_ms(delays::WAIT_AFTER_RESET_MS));
+        cassette::block_on(delay.delay_ms(delays::AFTER_RESET_MS));
 
         Self {
             spi,
             cs,
-            reset,
+            _reset: reset,
             irq,
             delay,
             buffer: [0u8; 20],
         }
     }
 
-    //pub async fn init() {}
+    /// Write a raw SDEP message to the device
+    async fn write_sdep_message<'a>(
+        &mut self,
+        msg: sdep::Message<'a>,
+    ) -> Result<(), Error<SPI::Error>> {
+        self.cs.set_low().unwrap();
+        self.delay.delay_us(delays::CS_TO_SCK_US).await;
+
+        self.spi
+            .transmit(msg.to_bytes(&mut self.buffer))
+            .map_err(|source| Error::SpiBus { source })?;
+
+        self.cs.set_high().unwrap();
+        self.delay.delay_us(delays::AFTER_SDEP_WRITE_US).await;
+
+        Ok(())
+    }
+
+    /// Write a raw SDEP message to the device
+    async fn read_sdep_message(&mut self) -> Result<sdep::Message, Error<SPI::Error>> {
+        self.cs.set_low().unwrap();
+        self.delay.delay_us(delays::CS_TO_SCK_US).await;
+
+        let buffer = &mut self.buffer;
+        self.spi
+            .receive(buffer)
+            .map_err(|source| Error::SpiBus { source })?;
+
+        self.cs.set_high().unwrap();
+        self.delay.delay_us(delays::AFTER_SDEP_READ_US).await;
+
+        sdep::Message::from_bytes(buffer).map_err(|source| Error::Sdep { source })
+    }
+
+    /// Sends the SDEP initialize command, which causes the board to reset.
+    /// This command should complete in under 1s.
+    pub async fn init(&mut self) -> Result<(), Error<SPI::Error>> {
+        let result = self
+            .write_sdep_message(sdep::Message::Command {
+                id: sdep::CommandType::Initialize.into(),
+                payload: &[],
+                more_data: false,
+            })
+            .await;
+
+        self.delay.delay_ms(delays::AFTER_INIT_MS).await;
+
+        result
+    }
+}
+
+/// The error type of this crate
+#[derive(Debug, Snafu)]
+pub enum Error<SpiError: snafu::AsErrorSource> {
+    /// An SPI bus error
+    SpiBus {
+        /// The underlying error
+        source: SpiError,
+    },
+    /// An SDEP protocol error
+    Sdep {
+        /// The underlying error
+        source: sdep::Error,
+    },
 }
